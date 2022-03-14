66d203b260c246d1674a0d8947fad162b653f3d9
gitmaus1 <agust.johannsson.95@gmail.com>
2022-03-14 16:22:53 +0000

    nah nha

---
+++ Verkefni/pithon/æfa3.py
@@ -0,0 +1,18 @@
+noooo="nooooooooooooooooooooooooooooooooooooooo bill telst"
+print(len(noooo))
+gaaser=input("bill")
+daaser=input("dill")
+if len(gaaser)==len(daaser):
+    print("a a a")
+else:
+    print("ha aha ahah")
+
+print("0123456789")
+dassss=int(input("0 til 9: "))
+kaaaaa="abcdefghjk"
+print(kaaaaa[dassss])
+email = input("slá in netfang: ")
+if email[-3:] == ".is" and "@" in email:
+    print("rétt íslenskt netfang")
+else:
+    print("ekki rét netfang")
