6f0e92244e0c449477f779cd92230135daea4ec6
gitmaus1 <agust.johannsson.95@gmail.com>
2022-03-23 15:57:16 +0000

    slka

---
+++ Verkefni/pithon/æfa7.py
@@ -0,0 +1,12 @@
+tölur=[2,4,7,8]
+print(tölur)
+tölur.append(100)
+print(tölur)
+tölur.insert(0,1)
+print(tölur)
+tölur.pop()
+print(tölur)
+del tölur[0]
+print(tölur)
+tölur.remove(7)
+print(tölur)
