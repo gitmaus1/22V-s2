8aad6062d639aa65920df99fdf359b5b5f09055e
gitmaus1 <agust.johannsson.95@gmail.com>
2022-03-28 20:44:36 +0100

    abc

---
+++ Verkefni/pithon/verk 3.1.py
@@ -0,0 +1,20 @@
+from dataclasses import replace
+from itertools import count
+from random import*
+from tkinter import END
+from math import*
+from random import randint
+import random
+from collections import Counter
+import collections
+
+#######################################################################
+svar1=input(": ")
+#svor1=print(Counter(svar1))
+print(Counter(svar1))
+svor=count()
+
+
+#results = collections.Counter(svar1)
+#print(results)
+#######################################################################
