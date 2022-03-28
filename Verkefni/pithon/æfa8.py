9bbc5e1cd6a6fa96cae31af7a5b5eda221414a14
gitmaus1 <agust.johannsson.95@gmail.com>
2022-03-28 01:20:37 +0100

    rth

--- Verkefni/pithon/æfa8.py
+++ Verkefni/pithon/æfa8.py
@@ -6,17 +6,6 @@ from random import randint
 import random
 
 
-listmist=[]
-
-daft=random.randint(5,25)
-
-
-soft=daft*10
-
-
-listmist.append(soft)
-
-print(listmist)
 
 
 
@@ -42,3 +31,25 @@ sort= sorted(thislist)
 print(sort)
 
 
+
+soft=[random.randint(0,10) for i in range(15)]
+
+snort= sorted(soft)
+
+print(snort)
+
+print("Largest element is:", snort[-1])
+print("Smalest element is:", snort[0])
+sum=0
+for k in range(0, len(snort)):
+    sum = sum + snort[k]
+print("suma er ",sum)
+x = len(snort)
+print("listin er ",x," langur")
+
+tort=[random.randint(0,100) for h in range(20)]
+print(tort)
+start=[]
+start.append(tort[0])
+start.append(tort[-1])
+print(start)
