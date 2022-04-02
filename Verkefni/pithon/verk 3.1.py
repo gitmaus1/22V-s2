0a3db99499cc6c81f6a69d9ff92f29742ecc0494
gitmaus1 <agust.johannsson.95@gmail.com>
2022-04-02 17:20:06 +0100

    erfg

--- Verkefni/pithon/verk 3.1.py
+++ Verkefni/pithon/verk 3.1.py
@@ -97,89 +97,117 @@ from unicodedata import digit
 
 #######################################################################
 #1
-svar1=input("svar1: ")
+#svar1=input("svar1: ")
 #svor1=print(Counter(svar1))
-print(Counter(svar1))
-svor=count()
+#print(Counter(svar1))
+#svor=count()
 
 
 #results = collections.Counter(svar1)
 #print(results)
 #######################################################################
 #2
-def aframback(s):
-    return s == s[::-1]
+#def aframback(s):
+#    return s == s[::-1]
  
   
 
-s = input("svar2: ")
-ans = aframback(s)
+#s = input("svar2: ")
+#ans = aframback(s)
  
-if ans:
-    print(s," er samhverfur strengur")
-else:
-    print(s," er ekki samhverfur strengur")
+#if ans:
+#    print(s," er samhverfur strengur")
+#else:
+#    print(s," er ekki samhverfur strengur")
 
 
 #######################################################################
 #3
+# kovid flensa     : koflidsa
+#svar3=input("svar3: ")
+#svar32=input("svar3: ")
 
-svar3=input("svar3: ")
-svar32=input("svar3: ")
-if len(svar3) >= 5 and len(svar32) >= 5:
+#if len(svar3) >= 5 and len(svar32) >= 5:
 
-      stuff=svar3[:2]+svar32[:-2]
-      print(stuff)
-      ruff=stuff.upper()
-      print(ruff)
-      duff=stuff.lower()
-      print(duff)
-      print(len(stuff))
-else:
-      print("error")
+#      stuff=svar3[:2]+svar32[:2]+svar3[-2]+svar3[-1]+svar32[-2]+svar32[-1]
+ #     print("Nýji strengurinn er: ",stuff)
+  #    ruff=stuff.upper()
+   #   print("Nýji strengurinn í upphafsstöfum: ",ruff)
+    #  duff=stuff.lower()
+     # print("Nýji strengurinn í lágstöfum: ",duff)
+      #print("Lengd strengjanna er",len(svar3)+len(svar32),"til samans")
+#      print("Lengd strengjanna er",len(stuff)," í nía orðinu")
+
+#else:
+#      print("error")
       
 
+#svarf = "á"
+#while svarf == "á":
+    #teljary += 1
+#    print("Sláðu inn 1 til að :")
+ #   print("Sláðu inn 2 til að fara aftur á birjun: ")
+  #  val = int(input(": "))
+
+   # if(val==1):
+    #    svar3=input("svar3: ")
+     #   svar32=input("svar3: ")
+
+      #  if len(svar3) >= 5 and len(svar32) >= 5:
+
+       #     stuff=svar3[:2]+svar32[:2]+svar3[-2]+svar3[-1]+svar32[-2]+svar32[-1]
+        #    print("Nýji strengurinn er: ",stuff)
+         #   ruff=stuff.upper()
+          #  print("Nýji strengurinn í upphafsstöfum: ",ruff)
+           # duff=stuff.lower()
+            #print("Nýji strengurinn í lágstöfum: ",duff)
+#            print("Lengd strengjanna er",len(svar3)+len(svar32),"til samans")
+ #           print("Lengd strengjanna er",len(stuff)," í nía orðinu")
+  #  elif(val==2):  
+   #  svarf = "nei"
+    #else:
+#     print("willa") 
 
 
 #######################################################################
 #4
-svar4=input("svar4: ")
-stem=0
-daft=len(svar4)
+#svar4=input("svar4: ")
+#stem=0
+#daft=len(svar4)
 #print(len(svar4))
-print(svar4)
-while 0 <= daft:
-    nuna=svar4[:-0] + svar4[:stem]
-    print(nuna)
-    stem-=1
-    daft-=1
+#print(svar4)
+#while 0 <= daft:
+#    nuna=svar4[:-0] + svar4[:stem]
+#    print(nuna)
+#    stem-=1
+#    daft-=1
 #######################################################################
 #5
-svar5=input("svar5: ")
-svor5=svar5.swapcase() 
-swore5=svor5.replace("1", "#") 
-swore52=swore5.replace("2", "#") 
-swore53=swore52.replace("3", "#") 
-swore54=swore53.replace("4", "#") 
-swore55=swore54.replace("5", "#") 
-swore56=swore55.replace("6", "#") 
-swore57=swore56.replace("7", "#") 
-swore58=swore57.replace("8", "#") 
-swore59=swore58.replace("9", "#") 
+#svar5=input("svar5: ")
+#svor5=svar5.swapcase() 
+#swore5=svor5.replace("1", "#") 
+#swore52=swore5.replace("2", "#") 
+#swore53=swore52.replace("3", "#") 
+#swore54=swore53.replace("4", "#") 
+#swore55=swore54.replace("5", "#") 
+#swore56=swore55.replace("6", "#") 
+#swore57=swore56.replace("7", "#") 
+#swore58=swore57.replace("8", "#") 
+#swore59=swore58.replace("9", "#") 
 
-print(swore59)
+#print(swore59)
 #######################################################################
 #6
-dem=0
-stemm=0
-svar6=input("svar6: ")
-daftt=len(svar6)
-print(svar6)
-while 0 <= daftt:
-    daftt-=1
-    print (svar6 [dem:stemm])
-    dem+=1
-    stemm-=1
+#dem=0
+#stemm=0
+#svar6=input("svar6: ")
+#daftt=len(svar6)
+#print(svar6)
+#while 0 <= daftt:
+#    daftt-=1
+#    print (svar6 [dem:stemm])
+#    dem+=1
+#    stemm-=1
 #######################################################################
 teljary=0
 svar = "já"
@@ -193,39 +221,53 @@ while svar == "já" or svar == "Já":
     if(val==1):
         svar1=input("svar1: ")
         print(Counter(svar1))
-        svor=count()
+        #svor=count()
     elif(val==2):
         def aframback(s):
             return s == s[::-1]
-        s = input("svar2: ")
+        s = input("svar2: ").lower()
         ans = aframback(s)  
         if ans:
             print(s," er samhverfur strengur")
         else:
             print(s," er ekki samhverfur strengur")
     elif(val==3):
-        svar3=input("svar3: ")
-        svar32=input("svar3: ")
-        if len(svar3) >= 5 and len(svar32) >= 5:
+        svarf = "á"
+        while svarf == "á":
+            #teljary += 1
+            print("Sláðu inn 1 til að svara: ")
+            print("Sláðu inn 2 til að fara aftur á birjun: ")
+            valf = int(input(": "))
 
-            stuff=svar3[:2]+svar32[:-2]
-            print(stuff)
-            ruff=stuff.upper()
-            print(ruff)
-            duff=stuff.lower()
-            print(duff)
-            print(len(stuff))
-        else:
-            print("error")
+            if(valf==1):
+                svar3=input("svar3: ")
+                svar32=input("svar3: ")
+
+                if len(svar3) >= 5 and len(svar32) >= 5:
+
+                    stuff=svar3[:2]+svar32[:2]+svar3[-2]+svar3[-1]+svar32[-2]+svar32[-1]
+                    print("Nýji strengurinn er: ",stuff)
+                    ruff=stuff.upper()
+                    print("Nýji strengurinn í upphafsstöfum: ",ruff)
+                    duff=stuff.lower()
+                    print("Nýji strengurinn í lágstöfum: ",duff)
+                    print("Lengd strengjanna er",len(svar3)+len(svar32),"til samans")
+                    print("Lengd strengjanna er",len(stuff)," í nía orðinu")
+            elif(valf==2):  
+             svarf = "nei"
+            else:
+             print("willa") 
+             svarf = "nei"
+             
     elif(val==4):
         svar4=input("svar4: ")
-        stem=0
+        stemf=0
         daft=len(svar4)
-        print(svar4)
+        #4print(svar4)
         while 0 <= daft:
-            nuna=svar4[:-0] + svar4[:stem]
+            nuna=svar4[:0] + svar4[stemf:]
             print(nuna)
-            stem-=1
+            stemf+=1
             daft-=1
     elif(val==5):
         svar5=input("svar5: ")
@@ -248,10 +290,12 @@ while svar == "já" or svar == "Já":
         daftt=len(svar6)
         print(svar6)
         while 0 <= daftt:
-            daftt-=1
             print (svar6 [dem:stemm])
+            
+            
             dem+=1
             stemm-=1
+            daftt-=1
     elif(val==7):  
      svar = "nei"
     else:
