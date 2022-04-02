3e1092bbfeb76d1c7929cda2f16a805cb184b978
gitmaus1 <agust.johannsson.95@gmail.com>
2022-04-02 14:00:29 +0100

    asd

--- Verkefni/pithon/verk 3.1.py
+++ Verkefni/pithon/verk 3.1.py
@@ -181,3 +181,79 @@ while 0 <= daftt:
     dem+=1
     stemm-=1
 #######################################################################
+teljary=0
+svar = "já"
+while svar == "já" or svar == "Já":
+    teljary += 1
+    print("Sláðu inn 1 fyrir verk1 2 fyrir verk2 3 fyrir verk3 4 fyrir verk4 5 fyrir verk5 6 fyrir verk6:")
+    print("Sláðu inn 7 til að stöðva keyrslu forrits:")
+    val = int(input(": "))
+    print("valdir ",val)
+
+    if(val==1):
+        svar1=input("svar1: ")
+        print(Counter(svar1))
+        svor=count()
+    elif(val==2):
+        def aframback(s):
+            return s == s[::-1]
+        s = input("svar2: ")
+        ans = aframback(s)  
+        if ans:
+            print(s," er samhverfur strengur")
+        else:
+            print(s," er ekki samhverfur strengur")
+    elif(val==3):
+        svar3=input("svar3: ")
+        svar32=input("svar3: ")
+        if len(svar3) >= 5 and len(svar32) >= 5:
+
+            stuff=svar3[:2]+svar32[:-2]
+            print(stuff)
+            ruff=stuff.upper()
+            print(ruff)
+            duff=stuff.lower()
+            print(duff)
+            print(len(stuff))
+        else:
+            print("error")
+    elif(val==4):
+        svar4=input("svar4: ")
+        stem=0
+        daft=len(svar4)
+        print(svar4)
+        while 0 <= daft:
+            nuna=svar4[:-0] + svar4[:stem]
+            print(nuna)
+            stem-=1
+            daft-=1
+    elif(val==5):
+        svar5=input("svar5: ")
+        svor5=svar5.swapcase() 
+        swore5=svor5.replace("1", "#") 
+        swore52=swore5.replace("2", "#") 
+        swore53=swore52.replace("3", "#") 
+        swore54=swore53.replace("4", "#") 
+        swore55=swore54.replace("5", "#") 
+        swore56=swore55.replace("6", "#") 
+        swore57=swore56.replace("7", "#") 
+        swore58=swore57.replace("8", "#") 
+        swore59=swore58.replace("9", "#") 
+
+        print(swore59)
+    elif(val==6):
+        dem=0
+        stemm=0
+        svar6=input("svar6: ")
+        daftt=len(svar6)
+        print(svar6)
+        while 0 <= daftt:
+            daftt-=1
+            print (svar6 [dem:stemm])
+            dem+=1
+            stemm-=1
+    elif(val==7):  
+     svar = "nei"
+    else:
+     print("willa") 
+print("valmyndin var keyrð",teljary)
