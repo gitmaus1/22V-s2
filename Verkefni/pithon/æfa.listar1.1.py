2663618082b2a13a5f0f0dbdfaa721333c018a7c
gitmaus1 <agust.johannsson.95@gmail.com>
2022-04-04 16:57:10 +0100

    nana

---
+++ Verkefni/pithon/æfa.listar1.1.py
@@ -0,0 +1,12 @@
+from ast import Num
+from dataclasses import replace
+from itertools import count
+from random import*
+from tkinter import END, SW
+from math import*
+from random import randint
+import random
+from collections import Counter
+import collections
+from dataclasses import replace
+from unicodedata import digit
