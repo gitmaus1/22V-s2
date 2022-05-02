9cd31635338f2375fa92ac5cf75e1c16522cd154
gitmaus1 <agust.johannsson.95@gmail.com>
2022-05-02 01:52:36 +0100

    srd

--- Verkefni/pithon/lokaverk.1.1.py
+++ Verkefni/pithon/lokaverk.1.1.py
@@ -3,7 +3,8 @@ from dataclasses import replace
 from itertools import count
 from os import sep
 from random import*
-from tkinter import END, SW
+from tarfile import LENGTH_LINK
+from tkinter import END, SW, W
 from math import*
 from random import randint
 import random
@@ -12,41 +13,274 @@ import collections
 from dataclasses import replace
 from unicodedata import digit
 import math
+import re
+
 ##################################################################################
 #exp í exp eru tilraunir til að leisa verkefnin oft án comenta
+#hsaq=input()
+#work2=list(hsaq)
+#print(work2)
+#work3=[]
+
+
+#IntVar = int("".join(filter(str.isdigit, hsaq)))
+#print(IntVar)
+
+#test_list = [8,5, "h", (5), 5,5," " ]+work2
+#print(test_list)
+#work=list(test_list)
+#print(work)
+#res = 0
+#for ele in test_list:
+#    if(isinstance(ele, int)):
+#        work3.append(ele)
+#        res += ele
+
+#ses=0
+#for e in work3:
+    
+        
+#        ses += e
+
+
+
+#print("Summa talna er : " + str(ses))
+#print("Summa talna er : " + str(res))
+#print(work3)
+# printing result 
+#print("Summa talna er : " + str(res))
+##################################################
+svar4=input("svar4: ")
+
+
+int4 = int("".join(filter(str.isdigit, svar4)))
+print(int4)
+
+
+# þeta verkefni er martröð
+sum_digit = 0
+for x in svar4:
+    if x.isdigit():
+        sum_digit += int(x)
+
+
+print("Summa talna er : ", sum_digit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#skiftir út
+res1=re.sub(r'[A-Z]', '_', svar4) 
+res2 = re.sub(r'[" "]', "?", res1)
+
+
+utcoma=res1+res2
+
+print(res2)
+
+
+
+input()
+
+##################################################
+#def draw_hangman(wrong,board):
+     
+#    if wrong == 0:
+ #       print(board)
+         
+  #  elif wrong == 1:
+#        print ("     ______")
+ #       print ("    |/")
+  #      print ("    |")
+   #     print ("    |")
+#        print ("    |")
+ #       print ("   _|_")
+  #      print(board)
+ 
+#    elif wrong == 2:       
+ #       print ("     ______")
+  #      print ("    |/")
+   #     print ("    |   o  ")
+    #    print ("    |")
+     #   print ("    |")
+      #  print ("   _|_")
+       # print(board)   
+#    elif wrong == 3:       
+ #       print ("     ______")
+  #      print ("    |/")
+   #     print ("    |   o  ")
+    #    print ("    |   |")
+     #   print ("    |")
+      #  print ("   _|_")
+       # print(board)   
+#    elif wrong == 4:       
+ #       print ("     ______")
+  #      print ("    |/")
+   #     print ("    |  _o  ")
+    #    print ("    |   |")
+     #   print ("    |")
+      #  print ("   _|_")
+       # print(board) 
+#    elif wrong == 5:       
+ #       print ("     ______")
+  #      print ("    |/")
+   #     print ("    |  _o ")
+    #    print ("    |   |")
+     #   print ("    |  /")
+      #  print ("    |")
+       # print ("    |")
+        #print(board) 
+#    elif wrong == 6:       
+ #       print ("     ______")
+  #      print ("    |/")
+   #     print ("    |  _o_ ")
+    #    print ("    |   |")
+     #   print ("    |  /")
+      #  print ("   _|_")
+       # print(board)   
+#    elif wrong == 7:       
+ #       print ("     ______")
+  #      print ("    |/")
+   #     print ("    |  _o_ ")
+    #    print ("    |   |")
+     #   print ("    |  / \\")
+      #  print ("   _|_")
+         
+#    elif wrong == 8:
+ #       print ("     ______")
+  #      print ("    |/  |")
+   #     print ("    |  _o_ ")
+    #    print ("    |   |")
+     #   print ("    |  / \\")
+      #  print ("   _|_")
+       # print(board)
+        #print("þú tapar")     
+         
+#def get_letter(guesses):
+ 
+ #   ok = True
+  #  while ok:    
+   #     try:
+    #        letter = input("gefðu staff: ")
+     #       if letter in guesses:
+      #          print("hefur þegar verið notað")
+       #     elif not letter.isalpha():
+        #        print("akki stafur")
+         #   elif len(letter) == 1:
+          #      letter = letter.lower()
+           #     break
+#        except:
+ #           print("reindu aftur")
+  #  return letter        
+        
+#def set_up():
+ 
+#    answe = ["stack", "overflow", "rocks","pokemon", "hús","matur"]
+#    answer= random.choice(answe)
+    
+                        
+       
+#    board = ""
+ #   for ch in answer:
+  #      if ch == " ":
+   #         board = board + " "
+    #    else:
+     #       board = board + "-"
+     
+#    return answer,board   
+ 
+#def check_guess(answer,board,letter,wrong):
+ 
+ #   if letter not in answer:
+  #      wrong = wrong + 1 
+   # else:
+    #    for i in range(len(answer)):
+     #       if answer[i] == letter:
+      #          board = board[:i] + letter + board[i+1:]
+#    return board,wrong
+             
+#answer,board = set_up()
+#guesses = ""
+#wrong = 0
+ 
+#for i in range(27):
+ #   letter = get_letter(guesses)
+  #  guesses = guesses + letter
+   # board,wrong = check_guess(answer,board,letter,wrong)
+    #draw_hangman(wrong,board)
+#    if board == answer:
+ #       print("þú vinur")
+  #      break
+   # elif wrong == 8:
+    #    break
 
 #################################
 
-control="ja"
-suma=1
-ass=0
+#control="ja"
 
-while control == "ja":
 
-    talapart2=int(input("Sláðu inn tölu: "))
-    soft=talapart2
 
-    if talapart2==0:
-        control="stop"
+#listrin=[]
+#while control == "ja":
+#    suma2=-1
+#    suma=1
+#    listrin.clear()
+#    talapart2=int(input("Sláðu inn tölu: "))
+    
 
-    for i in range(talapart2):
+#    btrrbrbtebt=abs(talapart2)
+
+
+#    if talapart2==0:
+#        control="stop"
+
+#    for i in range(abs(talapart2)):
         
-        dass=soft*(i+suma)
-        soft=talapart2-1
+#     if talapart2>0:
+#        listrin.append(suma)
+#        suma+=1
+#        talapart2-=1
+#        def multiplyNumbers(lst):
+#            product = 1
+#            for x in lst:
+#                product = product * x
+#            return product
+#        print(multiplyNumbers(listrin))
+    
+#     if talapart2<0:
+#        listrin.append(suma2)
+#        suma2-=1
+#        talapart2+=1
+#        def multiplyNumbers(lst):
+#            product = 1
+#            for x in lst:
+#                product = product * x
+#            return product
+#        print(multiplyNumbers(listrin))
 
 
-        print(dass)
 
 
 
-        ass=suma*i
         
-        suma+=1
-        
-        das=ass*(i+suma)
-            
-        print(ass*suma)
-
 #################################
 #farðegar=int(input("Hvað eru margir skráðir í ferðina?: "))
 
@@ -93,14 +327,234 @@ else:
 
 ##################################################################################
 ##Liður 2 – Margföldunarruna
+control="ja"
+
+
+
+listrin=[]
+while control == "ja":
+    suma2=-1
+    suma=1
+    listrin.clear()
+    talapart2=int(input("Sláðu inn tölu: "))
+    
+
+    btrrbrbtebt=abs(talapart2)
+
+
+    if talapart2==0:
+        control="stop"
+
+    for i in range(abs(talapart2)):
+        
+     if talapart2>0:
+        listrin.append(suma)
+        suma+=1
+        talapart2-=1
+        def multiplyNumbers(lst):
+            product = 1
+            for x in lst:
+                product = product * x
+            return product
+        print(multiplyNumbers(listrin))
+    
+     if talapart2<0:
+        listrin.append(suma2)
+        suma2-=1
+        talapart2+=1
+        def multiplyNumbers(lst):
+            product = 1
+            for x in lst:
+                product = product * x
+            return product
+        print(multiplyNumbers(listrin))
 
 
 ##################################################################################
 ##Liður 3 – Snaran
+def draw_hangman(wrong,board):
+     
+    if wrong == 0:
+        print(board)
+         
+    elif wrong == 1:
+        print ("     ______")
+        print ("    |/")
+        print ("    |")
+        print ("    |")
+        print ("    |")
+        print ("   _|_")
+        print(board)
+ 
+    elif wrong == 2:       
+        print ("     ______")
+        print ("    |/")
+        print ("    |   o  ")
+        print ("    |")
+        print ("    |")
+        print ("   _|_")
+        print(board)   
+    elif wrong == 3:       
+        print ("     ______")
+        print ("    |/")
+        print ("    |   o  ")
+        print ("    |   |")
+        print ("    |")
+        print ("   _|_")
+        print(board)   
+    elif wrong == 4:       
+        print ("     ______")
+        print ("    |/")
+        print ("    |  _o  ")
+        print ("    |   |")
+        print ("    |")
+        print ("   _|_")
+        print(board) 
+    elif wrong == 5:       
+        print ("     ______")
+        print ("    |/")
+        print ("    |  _o ")
+        print ("    |   |")
+        print ("    |  /")
+        print ("    |")
+        print ("    |")
+        print(board) 
+    elif wrong == 6:       
+        print ("     ______")
+        print ("    |/")
+        print ("    |  _o_ ")
+        print ("    |   |")
+        print ("    |  /")
+        print ("   _|_")
+        print(board)   
+    elif wrong == 7:       
+        print ("     ______")
+        print ("    |/")
+        print ("    |  _o_ ")
+        print ("    |   |")
+        print ("    |  / \\")
+        print ("   _|_")
+         
+    elif wrong == 8:
+        print ("     ______")
+        print ("    |/  |")
+        print ("    |  _o_ ")
+        print ("    |   |")
+        print ("    |  / \\")
+        print ("   _|_")
+        print(board)
+        print("þú tapar")     
+         
+def get_letter(guesses):
+ 
+    ok = True
+    while ok:    
+        try:
+            letter = input("gefðu staff: ")
+            if letter in guesses:
+                print("hefur þegar verið notað")
+            elif not letter.isalpha():
+                print("akki stafur")
+            elif len(letter) == 1:
+                letter = letter.lower()
+                break
+        except:
+            print("reindu aftur")
+    return letter        
+        
+def set_up():
+ 
+    answe = ["stack", "overflow", "rocks","pokemon", "hús","matur"]
+    answer= random.choice(answe)
+    
+                        
+       
+    board = ""
+    for ch in answer:
+        if ch == " ":
+            board = board + " "
+        else:
+            board = board + "-"
+     
+    return answer,board   
+ 
+def check_guess(answer,board,letter,wrong):
+ 
+    if letter not in answer:
+        wrong = wrong + 1 
+    else:
+        for i in range(len(answer)):
+            if answer[i] == letter:
+                board = board[:i] + letter + board[i+1:]
+    return board,wrong
+             
+answer,board = set_up()
+guesses = ""
+wrong = 0
+ 
+for i in range(27):
+    letter = get_letter(guesses)
+    guesses = guesses + letter
+    board,wrong = check_guess(answer,board,letter,wrong)
+    draw_hangman(wrong,board)
+    if board == answer:
+        print("þú vinur")
+        break
+    elif wrong == 8:
+        break
 
 
 ##################################################################################
 ##Liður 4 – Texti
+svar4=input("svar4: ")
+
+
+int4 = int("".join(filter(str.isdigit, svar4)))
+print(int4)
+
+
+# þeta verkefni er martröð
+sum_digit = 0
+for x in svar4:
+    if x.isdigit():
+        sum_digit += int(x)
+
+
+print("Summa talna er : ", sum_digit)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#skiftir út
+res1=re.sub(r'[A-Z]', '_', svar4) 
+res2 = re.sub(r'[" "]', "?", res1)
+
+
+utcoma=res1+res2
+
+print(res2)
+
+
+
+input()
 
 
 ##################################################################################
